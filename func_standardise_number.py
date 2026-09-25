f302acabe2f70920e32be404ea3423bad85b4039
KarelinIvan <ivan.karelin.1993@mail.ru>
2026-09-25 18:37:57 +0300

    Добавлена проверка на количество введенных пользователем цифр, т.к. моб.номер РФ состоит из 11 цифр. О не ккоректном количестве пользователь получает сообщение

--- func_standardise_number.py
+++ func_standardise_number.py
@@ -1,5 +1,5 @@
 import re
-number_phone = input()
+number_phone = input("Введите номер мобильного телефона\n")
 
 
 def standardise_number(phone_number: str) -> str:
@@ -7,16 +7,20 @@ def standardise_number(phone_number: str) -> str:
      и приводит к единому формату записи """
 
     line = phone_number
+
+    # Очищаем строку от символов, оставляем только цифры
     line_clean = re.sub(r"\D", "", line)
     numbers = list(line_clean)
 
-    numbers[0] = "7"
-    print(*numbers)
-
-    phone = "".join(numbers)
-
-    formatter = f"+{phone[0]}({phone[1:4]}){phone[4:7]}-{phone[7:9]}-{phone[9:11]}"
-    print(formatter)
+    if len(numbers) == 11:
+        if numbers[0] == "8":
+            numbers[0] = "7"
+        phone = "".join(numbers)
+        formatter = f"+{phone[0]}({phone[1:4]}){phone[4:7]}-{phone[7:9]}-{phone[9:11]}"
+        print(formatter)
+    elif len(numbers) > 11 or len(number_phone) < 11:
+        print("Неверный формат мобильного номера телефлна РФ,"
+              "номер должен состоять из 11 цифр.")
 
 
 standardise_number(number_phone)
