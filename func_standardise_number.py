af36c0c24168d5a35bdacf953c08842c6fe6462e
KarelinIvan <ivan.karelin.1993@mail.ru>
2026-09-25 18:57:34 +0300

    Добавлена проверка на остуствие ввода пользователем букв

--- func_standardise_number.py
+++ func_standardise_number.py
@@ -6,21 +6,31 @@ def standardise_number(phone_number: str) -> str:
     """ Функция принимает строку с номером мобильного телефона
      и приводит к единому формату записи """
 
-    line = phone_number
+    # Проверяем не содержит ли строка быквы
+    has_letters = any(char.isalpha() for char in number_phone)
 
-    # Очищаем строку от символов, оставляем только цифры
-    line_clean = re.sub(r"\D", "", line)
-    numbers = list(line_clean)
+    numbers = []
 
-    if len(numbers) == 11:
-        if numbers[0] == "8":
-            numbers[0] = "7"
-        phone = "".join(numbers)
-        formatter = f"+{phone[0]}({phone[1:4]}){phone[4:7]}-{phone[7:9]}-{phone[9:11]}"
-        print(formatter)
-    elif len(numbers) > 11 or len(number_phone) < 11:
-        print("Неверный формат мобильного номера телефлна РФ,"
-              "номер должен состоять из 11 цифр.")
+    if has_letters:
+        print("Ошибка, в номере телефона не должно быть букв.")
+    else:
+        line = phone_number
+        # Очищаем строку от символов, оставляем только цифры
+        line_clean = re.sub(r"\D", "", line)
+        numbers = list(line_clean)
+
+        if len(numbers) == 11:
+            if numbers[0] == "8":
+                numbers[0] = "7"
+            phone = "".join(numbers)
+            formatter = (
+                f"+{phone[0]}({phone[1:4]}){phone[4:7]}"
+                f"-{phone[7:9]}-{phone[9:11]}"
+            )
+            print(formatter)
+        elif len(numbers) > 11 or len(number_phone) < 11:
+            print("Неверный формат мобильного номера телефлна РФ,"
+                  "номер должен состоять из 11 цифр.")
 
 
 standardise_number(number_phone)
